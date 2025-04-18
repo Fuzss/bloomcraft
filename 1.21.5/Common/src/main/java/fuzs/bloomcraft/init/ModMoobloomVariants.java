bfb48723317cf57295bfe59d1aaf874ea5e7faaf
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-18 14:02:53 +0200

    prepare 1.21.5 port

---
+++ 1.21.5/Common/src/main/java/fuzs/bloomcraft/init/ModMoobloomVariants.java
@@ -0,0 +1,53 @@
+package fuzs.bloomcraft.init;
+
+import fuzs.bloomcraft.world.entity.animal.FlowerMobVariant;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+public class ModMoobloomVariants {
+    public static final ResourceKey<FlowerMobVariant> DANDELION = register("dandelion");
+    public static final ResourceKey<FlowerMobVariant> POPPY = register("poppy");
+    public static final ResourceKey<FlowerMobVariant> BLUE_ORCHID = register("blue_orchid");
+    public static final ResourceKey<FlowerMobVariant> ALLIUM = register("allium");
+    public static final ResourceKey<FlowerMobVariant> AZURE_BLUET = register("azure_bluet");
+    public static final ResourceKey<FlowerMobVariant> RED_TULIP = register("red_tulip");
+    public static final ResourceKey<FlowerMobVariant> ORANGE_TULIP = register("orange_tulip");
+    public static final ResourceKey<FlowerMobVariant> WHITE_TULIP = register("white_tulip");
+    public static final ResourceKey<FlowerMobVariant> PINK_TULIP = register("pink_tulip");
+    public static final ResourceKey<FlowerMobVariant> OXEYE_DAISY = register("oxeye_daisy");
+    public static final ResourceKey<FlowerMobVariant> CORNFLOWER = register("cornflower");
+    public static final ResourceKey<FlowerMobVariant> LILY_OF_THE_VALLEY = register("lily_of_the_valley");
+    public static final ResourceKey<FlowerMobVariant> WITHER_ROSE = register("wither_rose");
+    public static final ResourceKey<FlowerMobVariant> TORCHFLOWER = register("torchflower");
+    public static final ResourceKey<FlowerMobVariant> BUTTERCUP = register("buttercup");
+    public static final ResourceKey<FlowerMobVariant> PINK_DAISY = register("pink_daisy");
+
+    private static ResourceKey<FlowerMobVariant> register(String path) {
+        return ModRegistry.REGISTRIES.makeResourceKey(ModRegistry.MOOBLOOM_VARIANT_REGISTRY_KEY, path);
+    }
+
+    public static void bootstrap(BootstrapContext<FlowerMobVariant> context) {
+        register(context, DANDELION, Blocks.DANDELION);
+        register(context, POPPY, Blocks.POPPY);
+        register(context, BLUE_ORCHID, Blocks.BLUE_ORCHID);
+        register(context, ALLIUM, Blocks.ALLIUM);
+        register(context, AZURE_BLUET, Blocks.AZURE_BLUET);
+        register(context, RED_TULIP, Blocks.RED_TULIP);
+        register(context, ORANGE_TULIP, Blocks.ORANGE_TULIP);
+        register(context, WHITE_TULIP, Blocks.WHITE_TULIP);
+        register(context, PINK_TULIP, Blocks.PINK_TULIP);
+        register(context, OXEYE_DAISY, Blocks.OXEYE_DAISY);
+        register(context, CORNFLOWER, Blocks.CORNFLOWER);
+        register(context, LILY_OF_THE_VALLEY, Blocks.LILY_OF_THE_VALLEY);
+        register(context, WITHER_ROSE, Blocks.WITHER_ROSE);
+        register(context, TORCHFLOWER, Blocks.TORCHFLOWER);
+        register(context, BUTTERCUP, ModBlocks.BUTTERCUP.value());
+        register(context, PINK_DAISY, ModBlocks.PINK_DAISY.value());
+    }
+
+    private static void register(BootstrapContext<FlowerMobVariant> context, ResourceKey<FlowerMobVariant> resourceKey, Block block) {
+        context.register(resourceKey, new FlowerMobVariant(ModRegistry.MOOBLOOM_ENTITY_TYPE, resourceKey, block));
+    }
+}
---
+++ 1.21.5/Common/src/main/java/fuzs/bloomcraft/init/ModRegistry.java
@@ -0,0 +1,62 @@
+package fuzs.bloomcraft.init;
+
+import fuzs.bloomcraft.Bloomcraft;
+import fuzs.bloomcraft.world.entity.animal.Cluckbloom;
+import fuzs.bloomcraft.world.entity.animal.FlowerMobVariant;
+import fuzs.bloomcraft.world.entity.animal.Moobloom;
+import fuzs.puzzleslib.api.init.v3.registry.RegistryManager;
+import fuzs.puzzleslib.api.init.v3.tags.TagFactory;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.phys.Vec3;
+
+public class ModRegistry {
+    public static final ResourceKey<Registry<FlowerMobVariant>> MOOBLOOM_VARIANT_REGISTRY_KEY = ResourceKey.createRegistryKey(
+            Bloomcraft.id("moobloom_variant"));
+    public static final ResourceKey<Registry<FlowerMobVariant>> CLUCKBLOOM_VARIANT_REGISTRY_KEY = ResourceKey.createRegistryKey(
+            Bloomcraft.id("cluckbloom_variant"));
+    public static final RegistrySetBuilder REGISTRY_SET_BUILDER = new RegistrySetBuilder().add(
+            MOOBLOOM_VARIANT_REGISTRY_KEY,
+            ModMoobloomVariants::bootstrap).add(CLUCKBLOOM_VARIANT_REGISTRY_KEY, ModCluckbloomVariants::bootstrap);
+
+    static final RegistryManager REGISTRIES = RegistryManager.from(Bloomcraft.MOD_ID);
+    public static final Holder.Reference<EntityType<Moobloom>> MOOBLOOM_ENTITY_TYPE = REGISTRIES.registerEntityType(
+            "moobloom",
+            () -> EntityType.Builder.of(Moobloom::new, MobCategory.CREATURE)
+                    .sized(0.9F, 1.4F)
+                    .eyeHeight(1.3F)
+                    .passengerAttachments(1.36875F)
+                    .clientTrackingRange(10));
+    public static final Holder.Reference<EntityType<Cluckbloom>> CLUCKBLOOM_ENTITY_TYPE = REGISTRIES.registerEntityType(
+            "cluckbloom",
+            () -> EntityType.Builder.of(Cluckbloom::new, MobCategory.CREATURE)
+                    .sized(0.4F, 0.7F)
+                    .eyeHeight(0.644F)
+                    .passengerAttachments(new Vec3(0.0, 0.7, -0.1))
+                    .clientTrackingRange(10));
+    public static final Holder.Reference<CreativeModeTab> CREATIVE_MODE_TAB = REGISTRIES.registerCreativeModeTab(
+            ModItems.BUTTERCUP);
+    public static final Holder.Reference<EntityDataSerializer<Holder<FlowerMobVariant>>> MOOBLOOM_VARIANT_ENTITY_DATA_SERIALIZER = REGISTRIES.registerEntityDataSerializer(
+            "moobloom_variant",
+            () -> EntityDataSerializer.forValueType(FlowerMobVariant.streamCodec(MOOBLOOM_VARIANT_REGISTRY_KEY)));
+    public static final Holder.Reference<EntityDataSerializer<Holder<FlowerMobVariant>>> CLUCKBLOOM_VARIANT_ENTITY_DATA_SERIALIZER = REGISTRIES.registerEntityDataSerializer(
+            "cluckbloom_variant",
+            () -> EntityDataSerializer.forValueType(FlowerMobVariant.streamCodec(CLUCKBLOOM_VARIANT_REGISTRY_KEY)));
+
+    static final TagFactory TAGS = TagFactory.make(Bloomcraft.MOD_ID);
+    public static final TagKey<Biome> HAS_BUTTERCUP_BIOME_TAG = TAGS.registerBiomeTag("has_buttercup");
+    public static final TagKey<Biome> HAS_PINK_DAISY_BIOME_TAG = TAGS.registerBiomeTag("has_pink_daisy");
+
+    public static void bootstrap() {
+        ModBlocks.bootstrap();
+        ModItems.bootstrap();
+    }
+}
