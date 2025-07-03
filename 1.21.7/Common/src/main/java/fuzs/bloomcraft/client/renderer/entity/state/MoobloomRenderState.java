078b317d4ec8c443d3c0e50abbd6bda663462161
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-03 11:10:08 +0200

    prepare 1.21.7 port

---
+++ 1.21.7/Common/src/main/java/fuzs/bloomcraft/client/renderer/entity/state/MoobloomRenderState.java
@@ -0,0 +1,21 @@
+package fuzs.bloomcraft.client.renderer.entity.state;
+
+import fuzs.bloomcraft.init.ModBlocks;
+import fuzs.bloomcraft.init.ModCluckbloomVariants;
+import fuzs.bloomcraft.init.ModRegistry;
+import fuzs.bloomcraft.world.entity.animal.FlowerMobVariant;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class MoobloomRenderState extends LivingEntityRenderState implements BlockStateCarrierRenderState {
+    public ResourceLocation textureLocation = FlowerMobVariant.transformTextureLocation(FlowerMobVariant.getTextureLocation(
+            ModRegistry.MOOBLOOM_ENTITY_TYPE,
+            ModCluckbloomVariants.BUTTERCUP));
+    public BlockState blockState = ModBlocks.BUTTERCUP.value().defaultBlockState();
+
+    @Override
+    public BlockState blockState() {
+        return this.blockState;
+    }
+}
